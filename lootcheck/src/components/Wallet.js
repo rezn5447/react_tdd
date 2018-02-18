2fc184e0eeb3ae00a39c1820f03a6d5d4a69bee2
Resdan Logan <rezn5447@gmail.com>
2018-02-18 11:53:18 -0800

    correct way to hook up a redux component and test it

---
+++ lootcheck/src/components/Wallet.js
@@ -0,0 +1,19 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+
+export class Wallet extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="header">Wallet Balance : {this.props.balance}</h3>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { balance } = this.state;
+  return balance;
+};
+
+export default connect(mapStateToProps, null)(Wallet);
