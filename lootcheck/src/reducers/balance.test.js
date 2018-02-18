77cfe82b840b8f99e2de82e69046712a38d25061
Resdan Logan <rezn5447@gmail.com>
2018-02-18 11:13:24 -0800

    create balanceReducer test

--- lootcheck/src/reducers/balance.test.js
+++ lootcheck/src/reducers/balance.test.js
@@ -1,4 +1,12 @@
 import blanceReducer from './balance';
 import * as constants from '../actions/constants';
 
-describe('balanceReducer', () => {});
+describe('balanceReducer', () => {
+  it('sets a balance', () => {
+    const balance = 10;
+
+    expect(
+      balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
+    ).toEqual(balance);
+  });
+});
