fed2d3690e8a25442d30e988c87643ceffb8f2ef
Resdan Logan <rezn5447@gmail.com>
2018-02-17 18:13:31 -0800

    lootcheck init

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'react-materialize';
 import Gift from './Gift';
+import { max_number } from '../helper';
 
 class App extends Component {
   constructor() {
@@ -13,8 +14,8 @@ class App extends Component {
   addGift = () => {
     const { gifts } = this.state;
     const ids = this.state.gifts.map(gift => gift.id);
-    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
-    gifts.push({ id: max_id + 1 });
+
+    gifts.push({ id: max_number(ids) + 1 });
 
     this.setState({ gifts });
   };
