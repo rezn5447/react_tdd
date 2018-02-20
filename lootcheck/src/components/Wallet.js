0bf82e6e57f269de1f24d4e3ebc7a0e6a199bf25
Resdan Logan <rezn5447@gmail.com>
2018-02-19 20:55:43 -0800

    materialize changes

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Input, Button, Icon } from 'react-materialize';
+import { Input, Row, Col, Button, Icon } from 'react-materialize';
 
 export class Wallet extends Component {
   render() {
@@ -8,6 +8,24 @@ export class Wallet extends Component {
       <div>
         <h3 className="balance">Wallet Balance: {this.props.balance}</h3>
         <br />
+        <Row>
+          <Input s={4} label="Amount" />
+          <Button
+            waves="light"
+            className="btn-deposit"
+            onClick={this.props.deposit}
+          >
+            Deposit
+          </Button>
+          <Button
+            waves="light"
+            className="btn-withdraw"
+            onClick={this.props.withdraw}
+            style={{ margin: '0 10px' }}
+          >
+            Withdraw
+          </Button>
+        </Row>
       </div>
     );
   }
